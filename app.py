339a021b5d67a56821a51d3a8f9eb5d7018f9cf1
MajPaji <sababi@kth.se>
2020-12-16 19:33:07 +0000

    Add Sign up functionality

--- app.py
+++ app.py
@@ -24,9 +24,32 @@ def book_review_club():
     return render_template("index.html", books=books)
 
 
-@app.route('/sign_up', methods=['GET','POST'])
+@app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
+
+    if request.method == "POST":
+
+        existing_username = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_username:
+            flash("Username already exists")
+            return redirect(url_for('sign_up'))
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+
+        mongo.db.users.insert_one(sign_up)
+        session["user"] = request.form.get("username").lower()
+
     return render_template("sign_up.html")
+
+
+@app.route('/term_and_conditions')
+def term_and_conditions():
+    return render_template("term_and_conditions.html")
 
 
 if __name__ == "__main__":
