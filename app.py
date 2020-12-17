a99941ff03b51eb35174f2c9f1873936635975af
MajPaji <sababi@kth.se>
2020-12-17 15:54:48 +0000

    Add Logout functionality

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def sign_up():
 
         mongo.db.users.insert_one(sign_up)
         session["user"] = request.form.get("username").lower()
+        return redirect(url_for('profile', username=session["user"]))
 
     return render_template("sign_up.html")
 
@@ -50,6 +51,48 @@ def sign_up():
 @app.route('/term_and_conditions')
 def term_and_conditions():
     return render_template("term_and_conditions.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        # check user in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        # check if user exist
+        if existing_user:
+            # check passowrd
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                return redirect(url_for('profile', username=session["user"]))
+            else:
+                flash("Incorect Username and/or Password")
+                return redirect(url_for('login'))
+        else:
+            flash("Incorect Username and/or Password")
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+
+    session.pop("user")
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
