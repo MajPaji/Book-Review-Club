b2d1de278ebbae35fb8a1ba9b3fd7d7a3bc097af
MajPaji <sababi@kth.se>
2020-12-22 13:06:41 +0000

    Style added to book comment-section

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown();
     $('.modal').modal();
+    $('.tooltipped').tooltip();
 
 
         
